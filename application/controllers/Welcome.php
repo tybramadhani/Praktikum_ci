dc62b370f1a4bc77b822a2970806555a38cadeb3
tybramadhani <56532136+tybramadhani@users.noreply.github.com>
2019-10-31 22:25:44 +0700

    TUGAS WEB 3 = Menampilkan tabel ke html

---
+++ application/controllers/Welcome.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class Welcome extends CI_Controller {
+	function __construct(){
+		parent::__construct();
+		// $config['allowed_types'] = 'gif|jpg|png';
+		$this->load->model('model_pertama');
+			$this->load->helper('url');
+	}
+	
+	function index(){
+		$data['datashop']=$this->model_pertama->getdata()->result();
+		$this->load->view('v_model',$data);
+	}
+}
--- application/models/Model_pertama.php
+++ application/models/Model_pertama.php
@@ -1,18 +1,10 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-// class model_pertama extends CI_Model{
- 
-
-//     public function getdata(){
-//         return $this->db->get('datashop')->result();
-//     }
-
 class model_pertama extends CI_Model{
 	
 	function getdata(){
-		$hasil=$this->db->query("SELECT * FROM datashop");
-		return $hasil;
+		return $this->db->get('datashop');
 	}
-
+	
 }
